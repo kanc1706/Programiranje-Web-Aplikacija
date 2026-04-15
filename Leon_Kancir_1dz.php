68c311f0d34b8c7f4c7ba54d24bd50ea2d9f76e3
kanc1706 <leon60k@gmail.com>
2026-04-15 15:48:00 +0200

    Add files via upload

---
+++ Leon_Kancir_1dz.php
@@ -0,0 +1,23 @@
+<?php
+$naslov = "Nogometne ikone";
+$autor = "Leon Kancir";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $naslov; ?></title>
+</head>
+<body>
+
+<?php
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradio/la je $autor.</p>";
+?>
+
+<a href="https://www.youtube.com" target="_blank">Posjeti Youtube</a>
+
+</body>
+</html>
+
+<!-- Naziv datoteke: vjezba1.php -->
